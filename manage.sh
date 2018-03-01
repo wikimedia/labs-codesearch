2a105c4642a7c1dee437582e3981e8540772037b
Kunal Mehta <legoktm@member.fsf.org>
2018-02-28 20:52:32 -0800

    Merge enable.sh and start.sh into manage.sh
    Usage: MODE=start/enable/restart ./manage.sh
    
    Oh and add ArmchairGM which I forgot from the last commit.
    
    Change-Id: I0c085b1b8b4807d2328be8f1f551db3b29148d07

---
+++ manage.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+set -u
+
+MODE=${MODE:-start}
+
+systemctl $MODE hound-search
+systemctl $MODE hound-core
+systemctl $MODE hound-extensions
+systemctl $MODE hound-skins
+systemctl $MODE hound-things
+systemctl $MODE hound-ooui
+systemctl $MODE hound-operations
+systemctl $MODE hound-armchairgm
+systemctl $MODE hound_proxy
