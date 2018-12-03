82873fe35cc52983d5d39baaa6af2a274eb52545
Kunal Mehta <legoktm@member.fsf.org>
2018-12-02 22:28:11 -0800

    Change "Source code" link to point to Gitiles
    Change-Id: I0c39b2203f7ef30c8a67f08e67a5826af6a81adf

--- app.py
+++ app.py
@@ -174,7 +174,7 @@ def index(backend):
 <a href="https://www.mediawiki.org/wiki/codesearch">MediaWiki code search</a>
 is powered by <a href="https://github.com/etsy/hound">hound</a>.
 <br />
-<a href="https://phabricator.wikimedia.org/diffusion/LCSH/">Source code</a>
+<a href="https://gerrit.wikimedia.org/g/labs/codesearch">Source code</a>
 is available under the terms of the GPL v3 or any later version.
 </p>
 """
