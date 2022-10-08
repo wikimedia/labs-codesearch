df1dd5b5942850201ce5f49175bed42645b55799
Taavi Väänänen <hi@taavi.wtf>
2022-10-08 13:34:55 +0300

    wmcs: Add more toolforge repos
    Change-Id: I13c2308af9e5c6748e16cb4552eb788c7ba4af7f

--- write_config.py
+++ write_config.py
@@ -369,6 +369,11 @@ def make_conf(name, args, core=False, exts=False, skins=False, ooui=False,
         # toolforge infra
         conf['repos'].update(gerrit_prefix_list('operations/software/tools-'))
         conf['repos'].update(gerrit_prefix_list('cloud/toolforge/'))
+        # toolforge infra that's in the wrong place for legacy reasons
+        conf['repos']['labs/tools/maintain-kubeusers'] = repo_info('labs/tools/maintain-kubeusers')
+        conf['repos']['labs/tools/registry-admission-webhook'] = repo_info(
+            'labs/tools/registry-admission-webhook'
+        )
 
         # custom horizon panels, but not upstream code
         conf['repos'].update(gerrit_prefix_list('openstack/horizon/wmf-'))
