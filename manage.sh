ee81c4962682de7ecc1f509606c6d6ef3abf40cd
Timo Tijhof <krinkle@fastmail.com>
2023-03-25 17:30:45 -0700

    manage.sh: Add missing systemctl commands for various backends
    Change-Id: I07005107e7c2770f46aff6766b34e4f7c57f5f8f

--- manage.sh
+++ manage.sh
@@ -4,11 +4,13 @@ set -eu
 
 MODE=${MODE:-start}
 
+# Match "profile::codesearch::ports" in operations/puppet:/hiera/codesearch/common.yaml
 systemctl $MODE hound-search
-systemctl $MODE hound-core
+systemctl $MODE hound-search
 systemctl $MODE hound-extensions
 systemctl $MODE hound-skins
 systemctl $MODE hound-things
+systemctl $MODE hound-core
 systemctl $MODE hound-ooui
 systemctl $MODE hound-operations
 systemctl $MODE hound-armchairgm
@@ -17,3 +19,8 @@ systemctl $MODE hound-bundled
 systemctl $MODE hound-deployed
 systemctl $MODE hound-pywikibot
 systemctl $MODE hound-services
+systemctl $MODE hound-libraries
+systemctl $MODE hound-analytics
+systemctl $MODE hound-puppet
+systemctl $MODE hound-shouthow
+systemctl $MODE hound-wmcs
