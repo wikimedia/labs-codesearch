5cf79bfe456a6b9b51990537821f07035d92fd97
Amir Sarabadani <ladsgroup@gmail.com>
2023-12-11 12:45:46 +0100

    Add Github:wmde/new-lexeme-special-page
    Bug: T351938
    Change-Id: I6f1a2d74a8726821bd4a0832576d29b6f3b79235

--- write_config.py
+++ write_config.py
@@ -333,6 +333,7 @@ def make_conf(name, args, core=False, exts=False, skins=False, ooui=False,
         conf['repos'].update(gerrit_prefix_list('mediawiki/services/'))
         conf['repos']['mwaddlink'] = repo_info('research/mwaddlink')
         conf['repos']['Wikidata Query GUI'] = repo_info('wikidata/query/gui')
+        conf['repos']['New Lexeme form'] = gh_repo('wmde/new-lexeme-special-page')
         conf['repos']['Wikidata Query RDF'] = repo_info('wikidata/query/rdf')
         conf['repos']['iPoid'] = wmf_gitlab_repo('repos/mediawiki/services/ipoid')
 
